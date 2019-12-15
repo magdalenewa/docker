4e7cc645c8974cda4c819aefecddb8b2cae8a6e4
magdalenewa <57532484+magdalenewa@users.noreply.github.com>
2019-12-15 21:27:38 +0100

    Add files via upload

---
+++ powershell.ps1
@@ -0,0 +1,3 @@
+Write-Host "Congratulations! Your first script executed successfully"
+Invoke-Item C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe
+Start ".\ToscaCIRemoteExecutionService.exe"
